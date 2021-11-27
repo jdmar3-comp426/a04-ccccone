e6016cb34532552ae46a762a448dbb31974bb006
ccccone <fx00123@gmail.com>
2021-11-27 15:37:26 -0500

    create

--- server.js
+++ server.js
@@ -26,6 +26,14 @@ app.get("/app/", (req, res, next) => {
 
 // Define other CRUD API endpoints using express.js and better-sqlite3
 // CREATE a new user (HTTP method POST) at endpoint /app/new/
+app.post("/app/new/user", (req, res) => {
+	let username = req.body.user;
+	let password = req.body.pass;
+
+	const stmt = db.prepare('INSERT INTO userinfo (user, pass) VALUES (?, ?)');
+	const info = stmt.run(username, password);
+	res.status(200).json(info.changes);
+});
 
 // READ a list of all users (HTTP method GET) at endpoint /app/users/
 app.get("/app/users", (req, res) => {	
